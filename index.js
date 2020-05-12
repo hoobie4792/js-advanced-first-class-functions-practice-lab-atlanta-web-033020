b78a4834bb107533eff176e4c48a290993d5b200
github username <github email address>
2020-05-12 14:08:28 -0400

    Done.

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
 // Code your solution in this file!
-const logDriverNames = function(drivers) {
-	drivers.forEach(function(driver) {
+const logDriverNames = function (drivers) {
+	return drivers.forEach(function (driver) {
 		console.log(driver.name);
 	});
 };
 
-const logDriversByHometown = function(drivers, hometown) {
-	drivers.forEach(function(driver) {
-		if (driver.hometown === hometown) {
+const logDriversByHometown = function (drivers, location) {
+	return drivers.forEach(function (driver) {
+		if (driver.hometown === location) {
 			console.log(driver.name);
 		}
 	});
 };
 
-const driversByRevenue = function(drivers) {
-	return [ ...drivers ].sort(function(a, b) {
+const driversByRevenue = function (drivers) {
+	return drivers.slice().sort(function (a, b) {
 		return a.revenue - b.revenue;
 	});
 };
 
-const driversByName = function(drivers) {
-	return [ ...drivers ].sort(function(a, b) {
+const driversByName = function (drivers) {
+	return drivers.slice().sort(function (a, b) {
 		return a.name.localeCompare(b.name);
 	});
 };
 
-const totalRevenue = function(drivers) {
-	return drivers.reduce(function(total, driver) {
-		return (total += driver.revenue);
+const totalRevenue = function (drivers) {
+	return drivers.reduce(function (total, driver) {
+		return total + driver.revenue;
 	}, 0);
 };
 
-const averageRevenue = function(drivers) {
+const averageRevenue = function (drivers) {
 	return totalRevenue(drivers) / drivers.length;
 };
